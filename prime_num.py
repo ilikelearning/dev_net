23121a6ca9eef1e186f7b96404591acd965d22c8
Oluwafemi Famuyiwa <humble_sam@hotmail.com>
2024-10-05 01:18:21 +0100

    added a script (prime_num.py) to determine prime number

---
+++ prime_num.py
@@ -0,0 +1,36 @@
+
+
+def PrimeNo(x):
+    mylist = []
+    div_list = []
+    prime_no_check = []
+    prime_no_list = []
+    for i in range(int(x)+1):
+        mylist.append(i)
+
+    for div in mylist:
+        if div == 0 or div == 1:
+            continue
+        div_list.append(div)
+    
+    for num in mylist:
+        for denum in div_list:
+            if num % denum == 0 and num != 0:
+                prime_no_check.append(num)
+
+    with open("prime_output_sample.txt", "w") as f:
+        for check in prime_no_check:
+            if prime_no_check.count(check) == 1:
+                prime_no_list.append(check)
+        print("Number Sequence is: {}".format(mylist))
+        f.write("Number Sequence is: {}\n".format(mylist))
+        print(f"Prime Numbers in the sequence: {prime_no_list}")
+        f.write(f"Prime Number in the sequence: {prime_no_list}")
+    # print(f"Divisor list is {div_list}")
+
+if __name__ == "__main__":
+    print("A sequence will be created using your range value starting from 0")
+    range_val = input("Please provide your range value: ")
+    PrimeNo(range_val)
+
+
