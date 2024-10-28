3a270bd025488fbf7d477527a811e64e072970a3
Oluwafemi Famuyiwa <humble_sam@hotmail.com>
2024-10-28 11:15:20 +0000

    updated eos_query script and deleted old prime number script

--- eos_query.py
+++ eos_query.py
@@ -1,9 +1,11 @@
 import pyeapi
 from getpass import getpass
+from pprint import pprint 
 
 device_list = ['192.168.1.54', '192.168.1.56']
 
 user = input("username: ")
+
 passwd = getpass("Password: ")
 
 with open("eos_output_sample.txt", "w") as fl:
@@ -12,6 +14,7 @@ with open("eos_output_sample.txt", "w") as fl:
 		ip_addr = node.execute(["show ip interface brief"])
 		int_discard = node.execute(["show interfaces counters discards"])
 		version = node.execute(["show version"])
+		
 
 		print(f"\n{device} - EOS {version['result'][0]['version']} | IP Address Information")
 		print("-"*51)
@@ -23,7 +26,7 @@ with open("eos_output_sample.txt", "w") as fl:
 			print("| {} has ip address {}/{} configured".format(intf, ip_addr['result'][0]['interfaces'][intf]['interfaceAddress']['ipAddr']['address'], 
 			ip_addr['result'][0]['interfaces'][intf]['interfaceAddress']['ipAddr']['maskLen']))
 			fl.write(f"| {intf} has ip address {ip_addr['result'][0]['interfaces'][intf]['interfaceAddress']['ipAddr']['address']}/{ip_addr['result'][0]['interfaces'][intf]['interfaceAddress']['ipAddr']['maskLen']}\n")
-		
+
 		print("+")
 		fl.write("+\n")
 
@@ -31,3 +34,7 @@ with open("eos_output_sample.txt", "w") as fl:
 			print("| {} has {} Input Discards".format(intf,int_discard['result'][0]['interfaces'][intf]['inDiscards']))
 			fl.write(f"| {intf} has {int_discard['result'][0]['interfaces'][intf]['inDiscards']} Input Discards\n")
 
+# print('\n')
+# print("************","Dictionary Output","************","\n", sep='')
+# pprint(inventory_dic)
+# print('\n')
